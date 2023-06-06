a38237029c1bece5c6225561919c7faf098a8a9f
Anthony J Hernandez <hernandez.jg.anthony@gmail.com>
2023-06-06 11:40:46 -0500

    add test and ability to say 'hello' to any input

--- code.js
+++ code.js
@@ -3,29 +3,9 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 
-function sayHello() {
-}
-function sayHello() {
-    return '';
-}
 function sayHello(name) {
-    return "Hello, " + name + "!";
+    return `Hello, ${name}!`;
 }
-function sayHello(name) {
-    if (name === "Alex") {
-        return "Hello, Alex!";
-    } else {
-        return "Hello, Jane!";
-    }
-}
-function sayHello(name) {
-    if (name === "Alex") {
-        return "Hello, Alex!";
-    } else if (name === "Pat") {
-        return "Hello, Pat!";
-    } else {
-        return "Hello, Jane!";
-    }
-}
+
 
 
